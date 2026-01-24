b63ff7e6a7f78b559ab1722240ef41fbdb158925
PanArtur <pankovets.artur1@gmail.com>
2026-01-24 13:43:35 +0200

    setup redux store, MainLayout, App and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-function App() {
+import { loadFromLocalStorage } from "./app/store/authSlice.js";
+import { selectIsLoggedIn } from "./app/store/authSelectors.js";
+
+import { MainLayout } from "./app/MainLayout.jsx";
+import { HomePage } from "./features/home/HomePage.jsx";
+import { LocationsPage } from "./features/locations/pages/LocationsPage.jsx";
+import { LocationDetailsPage } from "./features/locations/pages/LocationDetailsPage.jsx";
+import { LocationFormPage } from "./features/locations/pages/LocationFormPage.jsx";
+import { ProfilePage } from "./features/profile/ProfilePage.jsx";
+import { LoginPage } from "./features/auth/LoginPage.jsx";
+import { RegisterPage } from "./features/auth/RegisterPage.jsx";
+import { NotFoundPage } from "./ui/NotFoundPage.jsx";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    dispatch(loadFromLocalStorage());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="locations" element={<LocationsPage />} />
+        <Route path="locations/:id" element={<LocationDetailsPage />} />
+        <Route
+          path="locations/add"
+          element={
+            isLoggedIn ? <LocationFormPage /> : <Navigate to="/auth/login" />
+          }
+        />
+        <Route
+          path="locations/:id/edit"
+          element={
+            isLoggedIn ? <LocationFormPage /> : <Navigate to="/auth/login" />
+          }
+        />
+        <Route
+          path="profile/:userId"
+          element={isLoggedIn ? <ProfilePage /> : <Navigate to="/auth/login" />}
+        />
+        <Route
+          path="auth/login"
+          element={!isLoggedIn ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="auth/register"
+          element={!isLoggedIn ? <RegisterPage /> : <Navigate to="/" />}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
