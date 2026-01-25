8a21edcd9d84888c23a20acf56820cb6a98dfd09
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-01-25 16:54:39 +0200

    Footer Desctop

--- src/app/MainLayout.jsx
+++ src/app/MainLayout.jsx
@@ -1,15 +1,9 @@
-import { Outlet } from "react-router-dom";
-import { Header } from "../ui/Header.jsx";
-import { Footer } from "../ui/Footer.jsx";
+import { Layout } from "../ui/Layout";
 
 export const MainLayout = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+      <Layout/>
     </>
   );
 };
