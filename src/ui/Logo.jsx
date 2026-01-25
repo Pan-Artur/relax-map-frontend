d8669065128f0f30e865c3570f8c0625259758af
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-01-25 22:13:16 +0200

    footer finish

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,8 +1,9 @@
 import style from './logo.module.css'
+import {ReactComponent as LogoIcon} from '../assets/icons/logo.svg'
 export const Logo = () => {
     return(
         <>
-            <img className={style.iconImg} src="https://i.ibb.co/dJ2hrqCh/map-search.png" alt="logo" />
+            <LogoIcon className={style.iconImg}/>
             <h2 className={style.iconTitle}>Relax Map</h2>
         </>
     )
--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -4,8 +4,8 @@ export const NavBar = () => {
     return(
         <nav className={style.navBar}>
             <ul className={style.list}>
-                <li className={style.item}><Link to="#" className={style.navLink}>Головна</Link> </li>
-                <li className={style.item}> <Link to="#" className={style.navLink}>Місця відпочинку</Link></li>
+                <li className={style.item}><Link to="/" className={style.navLink}>Головна</Link> </li>
+                <li className={style.item}> <Link to="/locations" className={style.navLink}>Місця відпочинку</Link></li>
             </ul>
         </nav>
     )
