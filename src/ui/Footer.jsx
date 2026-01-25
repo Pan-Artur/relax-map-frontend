67728db1260d713dad4792649d98a978e2fb2123
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-01-25 20:50:54 +0200

    footer ,mobile

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,16 +1,20 @@
 import style from './footer.module.css'
+import { Logo } from './Logo';
+import { NavBar } from './NavBar';
 export const Footer = () => {
   return (
     <footer className={style.footer}>
       <div className={style.footerContainer}>
         <div className={style.topFooterContainer}>
           <div className={style.logoContainer}>
-            logo
+            <Logo/>
           </div>
           <div className={style.socialContainer}>
-            fb  inst  X yt
+            <div>fb</div> <div>inst</div> <div>X</div> <div style={{ minWidth: "24px",minHeight: "24" }}>yt</div>
           </div>
-          <div className={style.footerNavBar}>NavBar</div>
+          <div className={style.footerNavBar}>
+            <NavBar/>
+          </div>
         </div>
         <div className={style.bottomFooterContainer}>
           <p className={style.FooterCopyright}>© 2025 Природні Мандри. Усі права захищені.</p>
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -2,7 +2,7 @@ import style from './logo.module.css'
 export const Logo = () => {
     return(
         <>
-            <img src="https://i.ibb.co/dJ2hrqCh/map-search.png" alt="logo" />
+            <img className={style.iconImg} src="https://i.ibb.co/dJ2hrqCh/map-search.png" alt="logo" />
             <h2 className={style.iconTitle}>Relax Map</h2>
         </>
     )
