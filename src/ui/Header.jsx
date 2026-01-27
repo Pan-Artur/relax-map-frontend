42bcb3734e5e3d304010618cfec63531271d6248
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-01-27 18:05:07 +0200

    userMenu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,6 +4,9 @@ import { Logo } from "./Logo.jsx";
 import { NavBar } from "./NavBar";
 import { Link } from "react-router-dom";
 import { Button } from "./Button.jsx";
+import { ReactComponent as BuregerMenu } from '../assets/icons/burger.svg';
+import { ReactComponent as LogOut} from '../assets/icons/LogOut.svg'
+import { UserMenu } from "./UserMenu.jsx";
 export const Header = ({isAuth,userAvatar,nameUser}) => {
   const [isOpen, setOpen] = useState(false);
   const [isOpenMenu,setOpenMenu] = useState(false)
@@ -20,26 +23,25 @@ export const Header = ({isAuth,userAvatar,nameUser}) => {
           <Logo />
         </div>
         <div className={style.NavBarAndbuttons}>
-          <NavBar isLogin={'#'} />
+          <NavBar isLogin={"kjh"} />
           <div className={style.desctopBtns } style={{display:isAuth ? "none" : "flex" }}>
             <Button/>
           </div>
             <div className={style.userInfo} style={{display: isAuth ? "flex" : "none"}}>
-                <button className={style.shareLactionBtn}>Поділитись локацією</button>
+                <button className={style.shareLoctionBtn}>Поділитись локацією</button>
                 <div className={style.UserAvatarAndName} onClick={openUserMenu}>
                     <img className={style.userAvatar} src={userAvatar} alt="UserAvatar" />
                     <p>{nameUser}</p>
                 </div>
-                <ul className={style.userList} style={{display:isOpenMenu ? "block" : "none"}}>
-                  <li className={style.userItem}><Link className={style.changeAvatar}>Зміна аватару</Link></li>
-                </ul>
-                <button className={style.logOutBtn}></button>
+                {isOpenMenu && <UserMenu />}
+                <button className={style.logOutBtn}> <LogOut/></button>
             </div>
         </div>
         <div className={style.mobileNavBarAndButtons}>
-          <div className={style.mobileBtns}>
+          <div className={style.mobileBtns} style={{display:isAuth ? "none" : "flex" }}>
             <Button/>
           </div>
+          <button className={style.shareLoctionBtn} style={{display: isAuth ? "flex" : "none"}}>Поділитись локацією</button>
         <div className={style.mobileNavContainer}>
           <button className={style.burgerMenu} onClick={openBurger}>
             <BuregerMenu className={style.burgerMenuIcon}/>
@@ -47,15 +49,22 @@ export const Header = ({isAuth,userAvatar,nameUser}) => {
 
           <ul className={`${isOpen ? style.mobileList : style.burgerMenuDisable}`}>
             <li className={style.mobileItem}>
-              <Link to={""} className={style.mobileNavLink}>Головна</Link>
+              <Link to={"/"} className={style.mobileNavLink}>Головна</Link>
             </li>
             <li className={style.mobileItem}>
-              <Link to={""} className={style.mobileNavLink}>Місця відпочинку</Link> 
+              <Link to={"/locations"} className={style.mobileNavLink}>Місця відпочинку</Link> 
             </li>
             <li className={style.mobileItem}>
-              <Link to={""} className={style.mobileNavLink}>Мій Профіль</Link>
+              <Link to={"/profile/:userId"} className={style.mobileNavLink} style={{display: isAuth ? "flex" : "none"}}>Мій Профіль</Link>
             </li>
-            <div className={style.burgerMobileBtns}>
+            <div className={style.UserAvatarAndName} onClick={openUserMenu} style={{display: isAuth ? "flex" : "none"}}>
+              <img className={style.userAvatar} src={userAvatar} alt="UserAvatar" />
+              <p>{nameUser}</p>
+            </div>
+            {isOpenMenu && <UserMenu />}
+            <button className={style.shareLoctionMobileBtn} style={{display: isAuth ? "flex" : "none"}}>Поділитись локацією</button>
+            <button className={style.logOutBtn} style={{display: isAuth ? "flex" : "none"}}> <LogOut/></button>
+            <div className={style.burgerMobileBtns} style={{display:isAuth ? "none" : "flex" }}>
               <Button/>
             </div>
           </ul>
--- src/ui/NavBar.jsx
+++ src/ui/NavBar.jsx
@@ -4,9 +4,9 @@ export const NavBar = ({isLogin}) => {
     return(
         <nav className={style.navBar} style={{width: isLogin ? '426px' : '275px'}}>
             <ul className={style.list}>
-                <li className={style.item}><Link to="#" className={style.navLink}>Головна</Link> </li>
-                <li className={style.item}> <Link to="#" className={style.navLink}>Місця відпочинку</Link></li>
-                <li className={style.item} style={{display: isLogin ? "block" : "none",}}><Link to="#" className={style.navLink}>Мій Профіль</Link></li>
+                <li className={style.item}><Link to="/" className={style.navLink}>Головна</Link> </li>
+                <li className={style.item}> <Link to="/locations" className={style.navLink}>Місця відпочинку</Link></li>
+                <li className={style.item} style={{display: isLogin ? "block" : "none",}}><Link to="profile/:userId" className={style.navLink}>Мій Профіль</Link></li>
             </ul>
         </nav>
     )
