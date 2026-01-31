3c23a62cc845936dd1cf2d9a39b93090e186a141
VoTs <cybulskij2011@ukr.net>
2026-01-31 21:06:47 +0200

    profile done (without adaptive)

--- src/features/profile/ProfilePage.jsx
+++ src/features/profile/ProfilePage.jsx
@@ -1,5 +1,17 @@
+import style from './ProfilePage.module.css'
+import { ProfileInfo } from '../../components/ProfileInfo/ProfileInfo'
+import { ProfilePlaceholder } from '../../components/ProfilePlaceholder/ProfilePlaceholder'
+
 export const ProfilePage = () => {
-  return (
-    <></>
-  );
-};
+	// TODO: articlesAmount logic
+
+	const articlesAmount = 0
+
+	return (
+		<div className={style.container}>
+			<ProfileInfo />
+
+			{articlesAmount === 0 ? <ProfilePlaceholder /> : null}
+		</div>
+	)
+}
