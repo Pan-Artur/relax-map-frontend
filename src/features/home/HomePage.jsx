5b6d21d565ba7fe9a60998f78b94d9582e292800
VoTs <cybulskij2011@ukr.net>
2026-02-01 13:18:27 +0200

    profile

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -8,7 +8,7 @@ export const HomePage = () => {
     <>
       <HeroBlock />
       <AdvantagesBlock />
-      {/* <PopularLocationsBlock /> */}
+      <PopularLocationsBlock />
       <ReviewsBlock />
     </>
   );
