921e185e5921ac66b1651f9e401e6cea04a013e5
PanArtur <pankovets.artur1@gmail.com>
2026-02-02 12:41:33 +0200

    fix local storage with authorization

--- src/app/store/authSlice.js
+++ src/app/store/authSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: null,
   token: null,
+  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
@@ -12,23 +13,29 @@ const authSlice = createSlice({
     login: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
+      state.isLoggedIn = true;
     },
+
     logout: (state) => {
       state.user = null;
       state.token = null;
+      state.isLoggedIn = false;
+      localStorage.removeItem("auth");
     },
+
     loadFromLocalStorage: (state) => {
       const saved = localStorage.getItem("auth");
 
       if (saved) {
         const parsed = JSON.parse(saved);
-
         state.user = parsed.user;
         state.token = parsed.token;
+        state.isLoggedIn = true;
       }
-    }
-  }
+    },
+  },
 });
+
 
 export const { login, logout, loadFromLocalStorage } = authSlice.actions;
 export default authSlice.reducer;
