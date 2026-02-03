26f9d7ede74af4d04ce8d166bae2548b0e2743ff
PanArtur <pankovets.artur1@gmail.com>
2026-02-03 12:17:30 +0200

    create page for location details

---
+++ src/features/locations/blocks/LocationDescription.jsx
@@ -0,0 +1,5 @@
+export const LocationDescription = () => {
+  return (
+    <></>
+  );
+};
--- src/features/locations/pages/LocationDetailsPage.jsx
+++ src/features/locations/pages/LocationDetailsPage.jsx
@@ -1,5 +1,45 @@
+import { useParams } from "react-router-dom";
+import { api } from "../../../app/services/apiClient";
+import { useEffect, useState } from "react";
+
+import { LocationInfoBlock } from "../blocks/LocationInfoBlock";
+import { LocationGallery } from "../blocks/LocationGallery";
+import { LocationDescription } from "../blocks/LocationDescription";
+import { ReviewsSection } from "../components/ReviewsSection";
+
 export const LocationDetailsPage = () => {
+  const { locationId } = useParams();
+
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        const res = await api.getLocationById(locationId);
+
+        setLocation(res.data);
+      } catch (error) {
+        setError("Failed to load location!");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchLocation();
+  }, [locationId]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!location) return null;
+
   return (
-    <></>
+    <>
+      <LocationInfoBlock location={location} />
+      <LocationGallery images={location.images} />
+      <LocationDescription description={location.description} />
+      <ReviewsSection locationId={location.id} />
+    </>
   );
 };
