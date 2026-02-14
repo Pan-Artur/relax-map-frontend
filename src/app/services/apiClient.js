9446e04ed11733bb1fc497c75ac811fc5f622c00
PanArtur <pankovets.artur1@gmail.com>
2026-02-14 20:46:31 +0200

    create host

--- src/app/services/apiClient.js
+++ src/app/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://relaxmap-backend.onrender.com/",
   headers: { "Content-Type": "application/json" },
 });
 
