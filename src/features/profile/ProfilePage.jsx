e1a81548728f2a199159d8055463d8d33de664f3
VoTs <cybulskij2011@ukr.net>
2026-02-14 19:52:14 +0200

    updated locations grid + location grid added to the profile

--- src/features/profile/ProfilePage.jsx
+++ src/features/profile/ProfilePage.jsx
@@ -1,42 +1,43 @@
-import { useEffect, useState } from "react";
-import { useParams, Navigate } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { useParams, Navigate } from 'react-router-dom'
 
-import { api } from "../../app/services/apiClient";
+import { api } from '../../app/services/apiClient'
 
-import { ProfileInfo } from "../../components/ProfileInfo/ProfileInfo";
-import { ProfilePlaceholder } from "../../components/ProfilePlaceholder/ProfilePlaceholder";
+import { ProfileInfo } from '../../components/ProfileInfo/ProfileInfo'
+import { ProfilePlaceholder } from '../../components/ProfilePlaceholder/ProfilePlaceholder'
+import { LocationsGrid } from '../locations/components/LocationsGrid'
 
-import style from "./ProfilePage.module.css";
+import style from './ProfilePage.module.css'
 
 export const ProfilePage = () => {
-  const { id } = useParams();
-  const [userInfo, setUserInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
+	const { id } = useParams()
+	const [userInfo, setUserInfo] = useState(null)
+	const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    if (!id) return;
+	useEffect(() => {
+		if (!id) return
 
-    api
-      .getUserById(id)
-      .then((res) => setUserInfo(res.data))
-      .catch((error) => {
-        console.error(error);
-        setUserInfo(null);
-      })
-      .finally(() => setLoading(false));
-  }, [id]);
+		api
+			.getUserById(id)
+			.then((res) => setUserInfo(res.data))
+			.catch((error) => {
+				console.error(error)
+				setUserInfo(null)
+			})
+			.finally(() => setLoading(false))
+	}, [id])
 
-  if (!id) return <Navigate to="/" />;
-  if (loading) return <div>Loading profile...</div>;
-  if (!userInfo) return <div>User not found</div>;
+	if (!id) return <Navigate to='/' />
+	if (loading) return <div>Loading profile...</div>
+	if (!userInfo) return <div>User not found</div>
 
-  // TODO: locations grid
+	return (
+		<div className={style.container}>
+			<ProfileInfo userId={id} user={userInfo} />
 
-  return (
-    <div className={style.container}>
-      <ProfileInfo userId={id} user={userInfo} />
+			<LocationsGrid id={id} />
 
-      {userInfo.locationsCount === 0 && <ProfilePlaceholder userId={id} />}
-    </div>
-  );
-};
+			{userInfo.locationsCount === 0 && <ProfilePlaceholder userId={id} />}
+		</div>
+	)
+}
