c3c79c935a1d3691c1f7b16f24898505d7308a8d
VoTs <cybulskij2011@ukr.net>
2026-02-15 14:39:05 +0200

    header logout button + modal

--- src/ui/ModalWrapper/ModalWrapper.jsx
+++ src/ui/ModalWrapper/ModalWrapper.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
+import { useEffect } from 'react'
 
 export const ModalWrapper = ({ children, onClose }) => {
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
+	useEffect(() => {
+		document.body.style.overflow = 'hidden'
 
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, []);
+		return () => {
+			document.body.style.overflow = ''
+		}
+	}, [])
 
-  return (
-    <div className="backdrop" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        {children}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='backdrop' onClick={onClose}>
+			<div className='modal' onClick={(e) => e.stopPropagation()}>
+				{children}
+			</div>
+		</div>
+	)
+}
