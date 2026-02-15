8ff0a10f5c9f6ae11a6d3b67d4d75ca950861369
PanArtur <pankovets.artur1@gmail.com>
2026-02-15 19:30:24 +0200

    fix

--- src/ui/NotFoundPage/NotFoundPage.jsx
+++ src/ui/NotFoundPage/NotFoundPage.jsx
@@ -1,15 +1,15 @@
 import { Container } from "../../components/Container/Container";
+import NotFound from '../../assets/icons/errorPage.png';
 import style from "./NotFoundPage.module.scss"
-
 export const NotFoundPage = () => {
-  // return (
-  //   <Container>
+  return (
+    <Container>
 
-  //     <h2 className={style.NotFoundTitle}>Ми шукали як могли...</h2>
-  //     <p className={style.NotFoundSubTitle}>Введіть правильний шлях</p>
-  //     <div className={style.NotFoundSvgContainer}>
-  //       <img style={{width:'300px'}} src={errorPage} alt="" />
-  //     </div>
-  //   </Container>
-  // );
+      <h2 className={style.NotFoundTitle}>Ми шукали як могли...</h2>
+      <p className={style.NotFoundSubTitle}>Введіть правильний шлях</p>
+      <div className={style.NotFoundSvgContainer}>
+        <img style={{width:'300px'}} src={NotFound} alt="" />
+      </div>
+    </Container>
+  );
 }
